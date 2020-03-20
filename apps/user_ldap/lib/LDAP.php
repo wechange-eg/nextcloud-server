8e97d3a35feb0adcc058962f11cd6af227f2ffb0
Arthur Schiwon <blizzz@arthur-schiwon.de>
2020-03-20 13:52:39 +0100

    fixup! use serverControls directly with LDAP calls, fixes 19127

--- apps/user_ldap/lib/LDAP.php
+++ apps/user_ldap/lib/LDAP.php
@@ -44,7 +44,7 @@ class LDAP implements ILDAPWrapper {
 	protected $pagedResultsAdapter;
 
 	public function __construct() {
-		if(version_compare(PHP_VERSION, '7.3', '<') === false) {
+		if(version_compare(PHP_VERSION, '7.3', '<') === true) {
 			$this->pagedResultsAdapter = new Php54();
 		} else {
 			$this->pagedResultsAdapter = new Php73();
